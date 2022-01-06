88bdc74d81fb6a940814f6e7750923678576c954
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2022-01-06 20:37:42 -0300

    adding initial style

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from "react";
+
+// import styles
+import "./styles/app.scss"
+
+// import components
 import Player from "./components/Player"; 
 import Song from "./components/Song"
 
