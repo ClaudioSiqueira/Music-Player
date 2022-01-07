1bb74fb123f9cc6560d211012a53aa89f6b57b5b
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2022-01-06 21:19:37 -0300

    Adding play and pause function

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,13 +2,18 @@ import React, { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({currentSong}) =>{
+const Player = ({currentSong, isPlaying, setIsPlaying}) =>{
     // Ref
     const audioRef = useRef(null)
 
     // Event Handlers
     const playSongHandler = () =>{
-        console.log(audioRef.current)
+        if(isPlaying){
+            audioRef.current.pause();
+        }else{
+            audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying)
     }
 
     return (
